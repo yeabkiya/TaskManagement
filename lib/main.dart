94eb72010ede00988c616b6fb77ebb3dcceba08a
Yeabsira Legesse <YEABSIRA1202@GMAIL.COM>
2024-10-03 13:21:48 -0400

    Set up the ThemeData and MaterialApp.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TaskManagerApp());
+}
+
+class TaskManagerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: TaskListScreen(),
+    );
+  }
+}
