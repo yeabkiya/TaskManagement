79ba3aae400efcf6bd6435408ddf1b236d765ace
Yeabsira Legesse <YEABSIRA1202@GMAIL.COM>
2024-10-03 13:56:18 -0400

    Added a toggle completion status function.

--- lib/main.dart
+++ lib/main.dart
@@ -26,18 +26,27 @@ class _TaskListScreenState extends State<TaskListScreen> {
   final TextEditingController _taskController = TextEditingController();
   List<Task> _task = [];
 
-  void _addTask() [
+  void _addTask() {
     if (_taskController.text.isNotEmpty) {
       setState(() {
         _tasks.add(Task(name: _taskController.text));
-        _taskController.clean();
+        _taskController.clear();
       });
     }
-  ]
+  }
 
   void _deleteTask(int index) {
     setState(() {
       _tasks.removeAt(index);
     });
   }
+
+  void _toggleTaskCompletion(int index) {
+    setState(() {
+      _tasks[index].isCompleted = !_tasks[index].isCompleted;
+    });
+  }
+
+  
+
 }
